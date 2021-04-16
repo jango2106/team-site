1b0ff1f5ff53e0b57aac62591241a85f306649e0
jango2106 <dustin.a.roan@gmail.com>
2021-04-16 14:32:13 -0500

    Add api init

---
+++ api/src/main/kotlin/com/lumen/storage/teamapi/TeamApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lumen.storage.teamapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamApiApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/lumen/storage/teamapi/TeamApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lumen.storage.teamapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
