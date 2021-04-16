4a6c77fa84f0f99fbfc1fdf7987861d1fa241f8c
jango2106 <dustin.a.roan@gmail.com>
2021-04-16 14:31:59 -0500

    Change git mocks and sorting of pulls

--- ui/src/store/modules/github/mutations.ts
+++ ui/src/store/modules/github/mutations.ts
@@ -3,6 +3,8 @@ import { GithubState, Pull } from "./types";
 
 export const mutations: MutationTree<GithubState> = {
   setPulls(state, payload: Array<Pull>) {
-    state.pulls = payload;
+    state.pulls = payload.sort((a, b) => {
+      return a.repoName.localeCompare(b.repoName) || a.pullNumber - b.pullNumber;
+    })
   },
 };
